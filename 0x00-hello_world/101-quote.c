a9c21fbbf86913fcb74db9a094162b471ab2567f
aymen baridi <baridiayman384@gmail.com>
2023-03-17 15:42:15 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using writ function.
+ *
+ * Return: Always 1
+*/
+int main(void)
+{
+	char qo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.qo.59);
+	return (1);
+}
