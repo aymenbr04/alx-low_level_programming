a3755303afead75875d5939886ba39f5f7c9383f
aymen baridi <baridiayman384@gmail.com>
2023-03-20 12:39:34 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	printf("%d is ", n);
+
 	if (n > 0)
-		printf("%i is positif\n", n);
-	else if (n == i)
-		printf("%i is zero\n", n);
+{
+		printf("%d is positive\n", n);
+}
+	else if (n == 0)
+{
+		printf("%d is zero\n", n);
+}
 	else
-		printf("%i is negative\n", n);
+}
+		printf("%d is negative\n", n);
+}
 	return (0);
 }
