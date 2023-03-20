9632d98cabfdb37803f6563b282c9f53d3f5701d
aymen baridi <baridiayman384@gmail.com>
2023-03-20 15:07:08 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase,
+ *		except q and e.
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+		if (c != 'e' && c != 'q')
+{
+		putchar(ch);
+}
+}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print digits number from base 10.
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+{
+		printf("%d", digit);
+}
+	printf("\n");
+	return (0);
+}
