551b88b0e22a0c9785ca4300f5bbf942696abf72
aymen baridi <baridiayman384@gmail.com>
2023-03-20 12:30:36 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-{
 		printf("%i is positif\n", n);
-}
 	else if (n == i)
-{
 		printf("%i is zero\n", n);
-}
 	else
-{
 		printf("%i is negative\n", n);
-}
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
  * main - entry point
  *
  * Description: print the value of n :
- *		greater than, is zero, is not less than 6.
+ *		greater than, is zero, is not less than 6..
  *
  * Return: 0 (success)
 */
@@ -16,13 +16,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	digit = n % 10;
+	printf("Last digit of %d is ", n);
 
-	if (digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("Last digit of %d is %d and is zero\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	if (n % 10 > 5)
+{
+		printf("%d and is greater than 5\n", n % 10);
+}
+	else if (n % 10 == 0)
+{
+		printf("%d and is 0\n", n % 10);
+}
+	else
+{
+		printf("%d and is less than 6 and not 0\n", n % 10);
+}
 	return (0);
 }
